171b846ac98297135485e36a3e80990e35f3dbf9
isabella-66 <amasprog@gmail.com>
2022-09-06 00:49:53 -0300

    feat: add initial files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EstudoASPNETRazor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
